c026722adaa0bca2eb82fb495194a3762de38fea
Adi Zafri <61119610+adizafri2000@users.noreply.github.com>
2024-04-15 02:41:02 +0800

    Update login.jsx

--- web/src/services/login.jsx
+++ web/src/services/login.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
-const host = import.meta.env.VITE_VM_IP
+const host = import.meta.env.VITE_API_URL
 // const baseUrl = `http://${host}/api/accounts/login`
-const baseUrl = '/api/accounts/login'
+const baseUrl = '/accounts/login'
 
 const login = async credentials => {
     const response = await axios.post(baseUrl, credentials)
