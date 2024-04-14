32c4f0dbdf4da11626a80221d67b50a607e10aec
Adi Zafri <adizafri2000@gmail.com>
2024-04-15 01:29:56 +0800

    Added WebConfig to return responses in JSON and did Sentry testing

---
+++ spring-boot-backend/src/main/java/com/example/springbootbackend/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.springbootbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(false).
+                ignoreAcceptHeader(true).
+                defaultContentType(MediaType.APPLICATION_JSON);
+    }
+}
