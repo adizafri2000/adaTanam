ec2e53b98e39033a935661808da20f3dc8416f79
Adi Zafri <61119610+adizafri2000@users.noreply.github.com>
2024-06-20 06:06:04 +0000

    update

--- spring-boot-backend/src/main/java/com/example/springbootbackend/repository/ProduceRepository.java
+++ spring-boot-backend/src/main/java/com/example/springbootbackend/repository/ProduceRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface ProduceRepository extends JpaRepository<Produce, Integer> {
 
-    List<Produce> findByStore(int storeId);
+    List<Produce> findByStore(Integer storeId);
 
 }
