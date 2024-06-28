03379ae40a966743d5c06201fe1146e7d9e0bff1
Adi Zafri <61119610+adizafri2000@users.noreply.github.com>
2024-06-28 10:00:19 +0000

    fix

--- spring-boot-backend/src/main/java/com/example/springbootbackend/dto/SearchResponseDTO.java
+++ spring-boot-backend/src/main/java/com/example/springbootbackend/dto/SearchResponseDTO.java
@@ -2,7 +2,7 @@ package com.example.springbootbackend.dto;
 import com.example.springbootbackend.dto.produce.ProduceResponseDTO;
 import com.example.springbootbackend.dto.store.StoreResponseDTO;
 
-public Record SearchResponseDTO(
+public record SearchResponseDTO(
   List<ProduceResponseDTO> produce,
   List<StoreResponseDTO> store
 ){}
